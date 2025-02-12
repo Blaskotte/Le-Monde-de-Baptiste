e1671611c795d5a5eb221a518e787df04a07828b
Blaskotte <wild.baptiste@gmail.com>
2025-02-12 18:19:43 +0800

    Ignore

--- Module ressources/Display/u8g2_GraphicTest/u8g2_GraphicTest.ino
+++ Module ressources/Display/u8g2_GraphicTest/u8g2_GraphicTest.ino
@@ -218,7 +218,9 @@
  */
  
 //U8G2_SSD1306_128X64_NONAME_1_4W_SW_SPI u8g2(U8G2_R0, /* clock=*/ 13, /* data=*/ 11, /* cs=*/ 10, /* dc=*/ 9, /* reset=*/ 8);
-U8G2_ST7920_128X64_1_SW_SPI u8g2(U8G2_R2, 13, 11, 10, 9);
+U8G2_ST7571_128X128_2_HW_I2C u8g2(U8G2_R0);
+
+
 
 void u8g2_prepare(void) {
   u8g2.setFont(u8g2_font_6x10_tf);
@@ -458,7 +460,7 @@ void setup(void) {
   /* U8g2 Project: KS0108 Test Board */
   //pinMode(16, OUTPUT);
   //digitalWrite(16, 0);	
-
+  u8g2.setI2CAddress(0x57);
   u8g2.begin();
   //u8g2.setFlipMode(0);
 }
