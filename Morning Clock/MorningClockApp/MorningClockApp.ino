bdc1b3a77837eacaf2fe9abdd608b97341695d21
Blaskotte <wild.baptiste@gmail.com>
2025-02-14 00:45:54 +0800

    Settings menu added

--- Morning Clock/MorningClockApp/MorningClockApp.ino
+++ Morning Clock/MorningClockApp/MorningClockApp.ino
@@ -15,13 +15,13 @@ const int RotarySW = 3;   //SW (Button function)
 SerialMP3Player mp3(/*RX*/ 6, /*TX*/ 7);
 
 //Bitmaps
-// 'bigHappy', 14x18px
 const unsigned char bigHappy_BM[] PROGMEM = {
+  // 'bigHappy', 14x18px
   0xc3, 0x30, 0xc3, 0x30, 0x0c, 0x0c, 0x0c, 0x0c, 0xf0, 0x03, 0xf0, 0x03, 0xf0, 0x03, 0xf0, 0x03, 0xf0, 0x03, 0xf0,
   0x03, 0xf0, 0x03, 0xf0, 0x03, 0xf0, 0x03, 0xf0, 0x03, 0x30, 0x03, 0x30, 0x03, 0x30, 0x03, 0x30, 0x03
 };
-// 'happyMenu', 8x9px
 const unsigned char happyMenu_BM[] PROGMEM = {
+  // 'happyMenu', 8x9px
   0x49, 0x22, 0x1c, 0x1c, 0x1c, 0x1c, 0x1c, 0x14, 0x14
 };
 const unsigned char happyMenu_select_BM[] PROGMEM = {
@@ -31,47 +31,61 @@ const unsigned char happyMenu_select_BM[] PROGMEM = {
 };
 const unsigned char alarmON_BM[] PROGMEM = {
   // 'alarmON, 8x9px
-  0x18, 0x3c, 0x7e, 0x7e, 0x7e, 0x7e, 0xff, 0x00, 0x18
+  0x42, 0xbd, 0x42, 0x89, 0x89, 0xb9, 0x81, 0x42, 0x3c
 };
-const unsigned char alarmON_select_BM [] PROGMEM = {
-  // 'AlarmON_select', 10x11px
-	0xfe, 0x01, 0xcf, 0x03, 0x87, 0x03, 0x03, 0x03, 0x03, 0x03, 0x03, 0x03, 0x03, 0x03, 0x01, 0x02, 
-	0xff, 0x03, 0xcf, 0x03, 0xfe, 0x01
+const unsigned char alarmON_select_BM[] PROGMEM = {
+  // 'alarmON_select', 10x11px
+  0xfe, 0x01, 0x7b, 0x03, 0x85, 0x02, 0x7b, 0x03, 0xed, 0x02, 0xed, 0x02, 0x8d, 0x02, 0xfd, 0x02,
+  0x7b, 0x03, 0x87, 0x03, 0xfe, 0x01
 };
 const unsigned char chimeON_BM[] PROGMEM = {
   // 'chimeON, 8x9px
-  0xe0, 0xfc, 0x9c, 0x84, 0x84, 0xc4, 0xe6, 0x67, 0x03
+  0x18, 0x3c, 0x7e, 0x7e, 0x7e, 0x7e, 0xff, 0x00, 0x18
 };
 const unsigned char chimeON_select_BM[] PROGMEM = {
   // 'chimeON_select, 10x11px
-  0xfe, 0x01, 0x3f, 0x02, 0x07, 0x02, 0xc7, 0x02, 0xf7, 0x02, 0xf7, 0x02, 0x77, 0x02, 0x33, 0x02,
-  0x31, 0x03, 0xf9, 0x03, 0xfe, 0x01
+  0xfe, 0x01, 0xcf, 0x03, 0x87, 0x03, 0x03, 0x03, 0x03, 0x03, 0x03, 0x03, 0x03, 0x03, 0x01, 0x02,
+  0xff, 0x03, 0xcf, 0x03, 0xfe, 0x01
 };
-const unsigned char clockICON_BM[] PROGMEM = {
-  // 'clockICON, 8x9px
-  0x42, 0xbd, 0x42, 0x89, 0x89, 0xb9, 0x81, 0x42, 0x3c
-};
-// 'alarmOFF', 10x11px
 const unsigned char alarmOFF_BM[] PROGMEM = {
-  0x19, 0x00, 0x3e, 0x00, 0x7c, 0x00, 0x7a, 0x00, 0x76, 0x00, 0x6e, 0x00, 0xdf, 0x00, 0x80, 0x00,
-  0x18, 0x00, 0x00, 0x00, 0x00, 0x00
+  // 'alarmOFF, 8x11px
+  0x01, 0x42, 0xbd, 0x46, 0x89, 0x99, 0xb9, 0xa1, 0x42, 0x7c, 0x80
 };
-// 'alarmOFF_select', 10x11px
 const unsigned char alarmOFF_select_BM[] PROGMEM = {
+  // 'alarmOFF_select', 10x11px
+  0xfc, 0x01, 0x7b, 0x03, 0x85, 0x02, 0x73, 0x03, 0xed, 0x02, 0xcd, 0x02, 0x8d, 0x02, 0xbd, 0x02,
+  0x7b, 0x03, 0x07, 0x03, 0xfe, 0x00
+};
+const unsigned char chimeOFF_BM[] PROGMEM = {
+  // 'chimeOFF', 8x9px
+  0x19, 0x3e, 0x7c, 0x7a, 0x76, 0x6e, 0xdf, 0x80, 0x18
+};
+const unsigned char chimeOFF_select_BM[] PROGMEM = {
+  // 'chimeOFF_select', 10x11px
   0xfe, 0x01, 0xcd, 0x03, 0x83, 0x03, 0x07, 0x03, 0x0b, 0x03, 0x13, 0x03, 0x23, 0x03, 0x41, 0x02,
   0xff, 0x02, 0xcf, 0x03, 0xfe, 0x01
 };
-// 'chimeOFF', 10x11px
-const unsigned char chimeOFF_BM[] PROGMEM = {
-  0xe1, 0x00, 0xfe, 0x00, 0x9c, 0x00, 0x8c, 0x00, 0x94, 0x00, 0xe4, 0x00, 0xe6, 0x00, 0x67, 0x00,
-  0x83, 0x00, 0x00, 0x00, 0x00, 0x00
-};
-// 'chimeOFF_select', 10x11px
-const unsigned char chimeOFF_select_BM[] PROGMEM = {
-  0xfe, 0x01, 0x3d, 0x02, 0x03, 0x02, 0xc7, 0x02, 0xe7, 0x02, 0xd7, 0x02, 0x37, 0x02, 0x33, 0x02,
-  0x31, 0x03, 0xf9, 0x02, 0xfe, 0x01
+const unsigned char right_arrow_BM[] PROGMEM = {
+  // 'right_arrow, 7x5px
+  0x08, 0x10, 0x3f, 0x10, 0x08
 };
 
+// 'bigHappy', 14x18px
+
+// 'happyMenu', 8x9px
+// 'happyMenu_select, 9x11px
+
+// 'alarmON, 8x9px
+// 'alarmON_select', 10x11px
+
+// 'chimeON, 8x9px
+// 'chimeON_select, 10x11px
+
+// 'alarmOFF, 8x11px
+// 'alarmOFF_select', 10x11px
+
+// 'chimeOFF', 8x9px
+// 'chimeOFF_select', 10x11px
 
 //Defining variables for rotary encoder and button
 int ButtonCounter = 0;            //counts the button clicks
@@ -98,6 +112,11 @@ bool happyMenuSelect = false;
 
 bool homePage = true;
 bool settingsPage = false;
+bool alarmPage = false;
+bool chimePage = false;
+bool clockPage = false;
+bool calendarPage = false;
+bool volumePage = false;
 
 unsigned int alarmHour = 0;
 unsigned int alarmMinute = 0;
@@ -157,6 +176,11 @@ void loop() {
     executeHomePage();
     printHomePage();
   }
+
+  if (homePage == false && settingsPage == true) {
+    updateSettingsPage();
+    executeSettingsPage();
+  }
 }
 
 
@@ -165,7 +189,6 @@ void printHomePage() {
   u8g2.firstPage();
   do {
 
-    // Page principale (alarmON/chimeON)
     u8g2.drawLine(0, 13, 128, 13);
 
     if (happyMenuSelect == true) {
@@ -185,7 +208,7 @@ void printHomePage() {
       if (chimeSelect == true) {
         u8g2.drawXBMP(27, 1, 10, 11, chimeOFF_select_BM);
       } else {
-        u8g2.drawXBMP(28, 2, 10, 11, chimeOFF_BM);
+        u8g2.drawXBMP(28, 2, 8, 9, chimeOFF_BM);
       }
     }
 
@@ -197,18 +220,17 @@ void printHomePage() {
       }
 
       // Display the time of the alarm
-      u8g2.setFont(u8g2_font_profont11_mn);
-      u8g2.setCursor(86, 10);
+      u8g2.setFont(u8g2_font_profont11_tf);
+      u8g2.setCursor(96, 10);
       u8g2.print(twoDigit(alarmHour));
       u8g2.print(':');
       u8g2.print(twoDigit(alarmMinute));
-      u8g2.drawXBMP(118, 2, 8, 9, clockICON_BM);
 
     } else {
       if (alarmSelect == true) {
         u8g2.drawXBMP(16, 1, 10, 11, alarmOFF_select_BM);
       } else {
-        u8g2.drawXBMP(17, 2, 10, 11, alarmOFF_BM);
+        u8g2.drawXBMP(17, 1, 8, 11, alarmOFF_BM);
       }
     }
 
@@ -221,10 +243,10 @@ void printHomePage() {
   } while (u8g2.nextPage());
 }
 
-void updateHomePage() { 
+void updateHomePage() {
   switch (RotateCounter) {
     case 1:
-      RotateCounter = 0;
+      RotateCounter = -3;
       break;
     case 0:
       chimeSelect = false;
@@ -251,7 +273,7 @@ void updateHomePage() {
       break;
 
     case -4:
-      RotateCounter = -3;
+      RotateCounter = 0;
       break;
 
     default:
@@ -259,17 +281,13 @@ void updateHomePage() {
   }
 }
 
-void executeHomePage() { 
+void executeHomePage() {
 
   if (buttonPressedState == true) {
     switch (RotateCounter) {
       case 0:
         break;
-
       case -1:
-        chimeSelect = true;
-        alarmSelect = false;
-        happyMenuSelect = false;
         if (chime_is_activated == true) {
           chime_is_activated = false;
         } else {
@@ -278,9 +296,6 @@ void executeHomePage() {
         break;
 
       case -2:
-        chimeSelect = false;
-        alarmSelect = true;
-        happyMenuSelect = false;
         if (alarm_is_activated == true) {
           alarm_is_activated = false;
         } else {
@@ -289,10 +304,160 @@ void executeHomePage() {
         break;
 
       case -3:
-        chimeSelect = false;
-        alarmSelect = false;
-        happyMenuSelect = true;
+        homePage = false;
+        settingsPage = true;
+        RotateCounter = 1;
         break;
+    }
+  }
+  buttonPressedState = false;  //reset this variable
+}
+
+
+
+void updateSettingsPage() {
+  switch (RotateCounter) {
+    case -10:
+      RotateCounter = 1;
+      break;
+    case -2:
+      RotateCounter = 1;
+      break;
+    case -1:
+      RotateCounter = 1;
+      break;
+    case 0:
+      RotateCounter = 1;
+      break;
+
+    case 1:
+      u8g2.firstPage();
+      do {
+        u8g2.drawXBMP(2, 2, 7, 9, happyMenu_BM);
+        u8g2.drawLine(0, 13, 127, 13);
+        u8g2.drawXBMP(5, 22, 7, 5, right_arrow_BM);
+        u8g2.setFont(u8g2_font_profont11_tf);
+        u8g2.drawUTF8(40, 10, "Réglages");
+        u8g2.drawUTF8(18, 28, "Réveil");
+        u8g2.drawStr(15, 43, "Carillon");
+        u8g2.drawStr(15, 58, "Horloge");
+      } while (u8g2.nextPage());
+      break;
+
+    case 2:
+      u8g2.firstPage();
+      do {
+        u8g2.drawXBMP(2, 2, 7, 9, happyMenu_BM);
+        u8g2.drawLine(0, 13, 127, 13);
+        u8g2.drawXBMP(5, 37, 7, 5, right_arrow_BM);
+        u8g2.setFont(u8g2_font_profont11_tf);
+        u8g2.drawUTF8(40, 10, "Réglages");
+        u8g2.drawUTF8(15, 28, "Réveil");
+        u8g2.drawStr(18, 43, "Carillon");
+        u8g2.drawStr(15, 58, "Horloge");
+      } while (u8g2.nextPage());
+      break;
+
+    case 3:
+      u8g2.firstPage();
+      do {
+        u8g2.drawXBMP(2, 2, 7, 9, happyMenu_BM);
+        u8g2.drawLine(0, 13, 127, 13);
+        u8g2.drawXBMP(5, 37, 7, 5, right_arrow_BM);
+        u8g2.setFont(u8g2_font_profont11_tf);
+        u8g2.drawUTF8(40, 10, "Réglages");
+        u8g2.drawStr(15, 28, "Carillon");
+        u8g2.drawStr(18, 43, "Horloge");
+        u8g2.drawStr(15, 58, "Calendrier");
+      } while (u8g2.nextPage());
+      break;
+
+    case 4:
+      u8g2.firstPage();
+      do {
+        u8g2.drawXBMP(2, 2, 7, 9, happyMenu_BM);
+        u8g2.drawLine(0, 13, 127, 13);
+        u8g2.drawXBMP(5, 37, 7, 5, right_arrow_BM);
+        u8g2.setFont(u8g2_font_profont11_tf);
+        u8g2.drawUTF8(40, 10, "Réglages");
+        u8g2.drawStr(15, 28, "Horloge");
+        u8g2.drawStr(18, 43, "Calendrier");
+        u8g2.drawStr(15, 58, "Volume");
+      } while (u8g2.nextPage());
+      break;
+
+    case 5:
+      u8g2.firstPage();
+      do {
+        u8g2.drawXBMP(2, 2, 7, 9, happyMenu_BM);
+        u8g2.drawLine(0, 13, 127, 13);
+        u8g2.drawXBMP(5, 37, 7, 5, right_arrow_BM);
+        u8g2.setFont(u8g2_font_profont11_tf);
+        u8g2.drawUTF8(40, 10, "Réglages");
+        u8g2.drawStr(15, 28, "Calendrier");
+        u8g2.drawStr(18, 43, "Volume");
+        u8g2.drawStr(15, 58, "Retour");
+      } while (u8g2.nextPage());
+      break;
+
+    case 6:
+      u8g2.firstPage();
+      do {
+        u8g2.drawXBMP(2, 2, 7, 9, happyMenu_BM);
+        u8g2.drawLine(0, 13, 127, 13);
+        u8g2.drawXBMP(5, 52, 7, 5, right_arrow_BM);
+        u8g2.setFont(u8g2_font_profont11_tf);
+        u8g2.drawUTF8(40, 10, "Réglages");
+        u8g2.drawStr(15, 28, "Calendrier");
+        u8g2.drawStr(15, 43, "Volume");
+        u8g2.drawStr(18, 58, "Retour");
+      } while (u8g2.nextPage());
+      break;
+
+    case 7:
+      RotateCounter = 6;
+      break;
+    case 8:
+      RotateCounter = 6;
+      break;
+    case 9:
+      RotateCounter = 6;
+      break;
+    case 16:
+      RotateCounter = 6;
+      break;
+  }
+}
+
+void executeSettingsPage() {
+
+  if (buttonPressedState == true) {
+    switch (RotateCounter) {
+    case 1:
+
+      break;
+
+    case 2:
+
+      break;
+
+    case 3:
+
+      break;
+
+    case 4:
+
+      break;
+
+    case 5:
+
+      break;
+
+    case 6:
+      settingsPage = false;
+      homePage = true;
+      RotateCounter = 0;
+      break;
     }
   }
   buttonPressedState = false;  //reset this variable
@@ -301,7 +466,7 @@ void executeHomePage() {
 
 void button_is_pressed() {
   TimeNow2 = millis();
-  if (TimeNow2 - TimeNow1 > 400) {
+  if (TimeNow2 - TimeNow1 > 200) {
     buttonPressedState = true;
   }
   TimeNow1 = millis();  //"reset" timer; the next 500 ms is counted from this moment
@@ -317,8 +482,10 @@ void wheel_is_rotated() {
     // the encoder is rotating CCW so increase
     if (digitalRead(RotaryDT) != CLKNow) {
       RotateCounter++;
+      delay(200);
     } else {
       RotateCounter--;
+      delay(200);
     }
   }
 
